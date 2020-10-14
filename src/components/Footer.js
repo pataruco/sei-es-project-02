50fae5dd1ec163b3d34498f3cb8b2fc08117433a
Ismael Gonzalez <ismamontiel@hotmail.com>
2020-10-14 21:37:44 +0200

    scaffolding and sass

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p> &copy Carmen, Jose Manuel & Ismael</p>
+      <p>SEI España 2020</p>
+    </footer>
+  );
+};
+
+export default Footer;
